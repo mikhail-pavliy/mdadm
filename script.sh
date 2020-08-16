a4d39901f81265af1837b6b7f3d6489b2d7b7c61
mikhail-pavliy <69155591+mikhail-pavliy@users.noreply.github.com>
2020-08-16 18:37:42 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,75 +1,74 @@
-#Входим под супер юзером
+	# Log in as a super user
+```bash
 sudo su
-
-# Собиравем RAID/10
+```
+	# Create RAID/10
+```bash
 sudo lshw -short | grep disk
-
-#3ануляем суперблоки
-
-mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
-
-#Создаем Raid следующей командой
-
-mdadm --create --verbose /dev/md0 -l 10 -n 4 /dev/sd{b,c,d,e,f}
-
-#Проверим что RAID собрался
-
+```
+	# Zeroing superblocks
+```bash
+mdadm --zero-superblock --force /dev/sd{b,c,d,e}
+```
+	# Create Raid following command
+```bash
+mdadm --create --verbose /dev/md0 -l 10 -n 4 /dev/sd{b,c,d,e}
+```
+	# Verify that the RAID is assembled
+```bash
 cat /proc/mdstat
-
-#Должно выйти следующее
-
-Personalities : [raid10]
-
-md0 : active raid10 sde[4] sdd[2] sdc[1] sdb[0]
-
-610304 blocks super 1.2 512K chunks 2 near-copies [4/4] [UUUU]
-
-#Просмотрим все диски:
-
-fdisk -l
-
-#Создем раздел GPT на неразмеченном диске /dev/sdb
-
-gdisk /dev/sdb
-
-n
-
-fd00
-
-w
-
-#копируем таблицу разделов на другие жесткие диски /dev/sdc, /dev/sdd, /dev/sde:
-
-sudo sgdisk -G /dev/sdc
-
-sudo sgdisk -R /dev/sdd /dev/sdb
-
-sudo sgdisk -G /dev/sdd
-
-sudo sgdisk -R /dev/sde /dev/sdb
-
-sudo sgdisk -G /dev/sde
-
-#отформатируем дисковый массив:
-
-sudo mkfs.ext4 /dev/md0
-
-#создаём файл mdadm.conf
-
-echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
-
-#и записываем в него информацию
-
+```
+	# The following should come out
+#Personalities : [raid10]
+#md0 : active raid10 sde[4] sdd[2] sdc[1] sdb[0]
+#610304 blocks super 1.2 512K chunks 2 near-copies [4/4] [UUUU]
+	# Let's look at all disks
+	# create mdadm.conf file
+```bash
+mkdir /etc/mdadm
+```
+```bash
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf```
+	# and write information into it
+```bash
 sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >>mdadm.conf
-
-#редактируем файл fstab для монтирования массива при запуске системы:
-
-
+```
+```bash
+fdisk -l
+```
+	# Create a GPT partition on an unallocated disk /dev/sdb
+```bash
+gdisk /dev/sdb
+```
+```bash
+n
+```
+```bash
+fd00
+```
+```bash
+w
+```
+	# copy the partition table to other hard drives /dev/sdc, /dev/sdd, /dev/sde:
+```bash
+sgdisk -G /dev/sdc
+sgdisk -R /dev/sdd /dev/sdb
+sgdisk -G /dev/sdd
+sgdisk -R /dev/sde /dev/sdb
+sgdisk -G /dev/sde
+```
+	# format the disk array
+```bash
+sudo mkfs.ext4 /dev/md0
+```
+	# edit the fstab file to mount the array at system startup
+```bash
 if ! grep -q 'md0' /etc/fstab ; then
     echo '# Raid10' >> /etc/fstab
     echo '/dev/md0 /mnt/md0 ext4 defaults 0 4' >> /etc/fstab
 fi
-
-#перезапусаем систему
-
+```
+	# restart the system
+```bash
 shutdown -r now
+```
